68e7f038a458fb5b0bef2c6b88ffd43c528de3e9
shekharpratapsingh59 <shekhar.pratap@jumkey.com>
2021-02-07 23:12:15 +0530

    added custom button

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import {Switch,Route} from 'react-router-dom'
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
-import Header from './components/header/header.component'
+import Header from './components/header/header.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import './App.css';
 
 
@@ -12,7 +13,8 @@ function App() {
       <Header/>
       <Switch>
       <Route exact path="/" component={Homepage} />
-      <Route path="/shop" component={ShopPage}/>
+        <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={ SignInAndSignUp}/>
      </Switch>
       
     </div>
